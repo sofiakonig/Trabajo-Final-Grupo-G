3103482e1e01311ab96aa4d75e8ffc4b46f1dd27
foieriagostina <agostinafoieri@gmail.com>
2023-10-01 21:29:26 -0300

    avances storage

--- storage-newslatter.js
+++ storage-newslatter.js
@@ -1,15 +1,31 @@
-const baseDeDatosCorreos = {
+/*const baseDeDatosCorreos = {
 correos: ["juanadearco@gmail.com", "pedrogarcia@gmail.com","martinamarquez@gmail.com", "emiliamartinez@gmail.com", "estebanripoll@gmail.com"]
 }
 
-// Almacenar la base de datos en localStorage
 localStorage.setItem('baseDeDatosCorreos', JSON.stringify(baseDeDatosCorreos));
 
-// Recuperar la base de datos desde localStorage
 const baseDeDatosRecuperada = JSON.parse(localStorage.getItem('baseDeDatosCorreos')) || {};
 
-// Acceder a la lista de correos electrónicos
 const listaDeCorreos = baseDeDatosRecuperada.correos || [];
 
-// Mostrar la lista de correos (solo para propósitos de demostración)
-console.log(listaDeCorreos);
+console.log(listaDeCorreos);*/
+
+document.getElementById('newslatter').addEventListener('submit', function(event){
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    // Obten el valor del input
+    let email = document.getElementById('barra').value;
+    
+    // Crea un objeto JSON con el string
+    let jsonCorreos = { "texto": email };
+
+    // Convierte el objeto JSON a una cadena
+    let jsonString = JSON.stringify(jsonCorreos);
+
+    // Puedes imprimir el JSON en la consola para verificar
+    console.log(jsonString);
+
+    // Aquí puedes hacer lo que quieras con la cadena JSON, por ejemplo, enviarla a un servidor
+
+    // Si deseas guardar el JSON localmente, puedes usar localStorage
+    localStorage.setItem('jsonCorreos', jsonString); })

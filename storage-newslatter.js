a516a7f3c734e8170526f92cd7084435a50b9a69
foieriagostina <agostinafoieri@gmail.com>
2023-10-03 19:00:42 -0300

    actualización

--- storage-newslatter.js
+++ storage-newslatter.js
@@ -13,7 +13,7 @@ console.log(listaDeCorreos);*/
 document.getElementById('newslatter').addEventListener('submit', function(event){
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Obten el valor del input
+    // registra el valor del input
     let email = document.getElementById('barra').value;
     
     // Crea un objeto JSON con el string
@@ -22,7 +22,7 @@ document.getElementById('newslatter').addEventListener('submit', function(event)
     // Convierte el objeto JSON a una cadena
     let jsonString = JSON.stringify(jsonCorreos);
 
-    // Puedes imprimir el JSON en la consola para verificar
+    // mostrar JSON en la consola para verificar
     console.log(jsonString);
 
     // Aquí puedes hacer lo que quieras con la cadena JSON, por ejemplo, enviarla a un servidor
